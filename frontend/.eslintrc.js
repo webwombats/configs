b9c50b7a74d5647fdebef0378c3bc54da9ee4220
Yuri Yakovlev <hi@mynameisyuri.com>
2020-06-13 00:40:05 +0200

    Add eslint config

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+  },
+};
